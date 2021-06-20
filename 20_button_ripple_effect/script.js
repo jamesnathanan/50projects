0349638bdeb0bb631ac654f5c6f3bafcb3fbfddb
jamesnathanan <57458881+jamesnathanan@users.noreply.github.com>
2021-06-21 00:13:43 +0700

    add ripple effect

--- 20_button_ripple_effect/script.js
+++ 20_button_ripple_effect/script.js
@@ -1,7 +1,7 @@
 const buttons = document.querySelectorAll('.ripple')
 
 buttons.forEach( button => {
-    button.addEventListener('click', (e) => {
+    button.addEventListener('click', function (e) {
         const x = e.clientX
         const y = e.clientY
 
@@ -9,9 +9,22 @@ buttons.forEach( button => {
 
         const buttonTop = e.target.offsetTop
         const buttonLeft = e.target.offsetLeft
-        const buttonRight = e.target.offsetRight
-        const buttonBottom = e.target.offsetBottom
 
-        console.log(buttonTop, buttonLeft)
+
+        const xInside = x - buttonLeft
+        const yInside = y - buttonTop
+
+        // console.log(xInside, yInside)
+
+        const circle = document.createElement('span')
+        circle.classList.add('circle')
+        circle.style.top = yInside + 'px'
+        circle.style.left = xInside + 'px'
+
+        this.appendChild(circle)
+        // this.appendChild only work with non arrow function
+
+        // we have to remove span we added manually by settimeout callback
+        setTimeout( () => circle.remove(), 500)
     })
 })
